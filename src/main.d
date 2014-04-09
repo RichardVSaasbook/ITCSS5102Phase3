4eeb99f3c2f83893be8fcf0c24b9d872c553b1a5
Richard Voelker <rvoelker@uncc.edu>
2014-04-09 19:40:28 -0400

    Created base Shell with Menu and setup RGBColor and HSLColor classes.

--- src/main.d
+++ src/main.d
@@ -1,5 +1,15 @@
+import org.eclipse.swt.widgets.Display;
+import phase3.gui.MainShell;
 
+void main () {
+	Display display = new Display;
+	MainShell shell = new MainShell(display);
 
-void main() {
-	
+	while (!shell.isDisposed) {
+		if (!display.readAndDispatch()) {
+			display.sleep();
+		}
+	}
+
+	display.dispose();
 }
